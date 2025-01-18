7b23d12be8360222430027dcf540ae5dad7f00bc
Arafat <arafat2k30@gmail.com>
2025-01-18 22:28:22 +0600

    covid app

---
+++ lib/view/bottom_page_view.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+class BottomPageView extends StatefulWidget {
+  const BottomPageView({super.key});
+
+  @override
+  State<BottomPageView> createState() => _BottomPageViewState();
+}
+
+class _BottomPageViewState extends State<BottomPageView> {
+  static  List<Widget> _widgetList=[
+    Text('data'),
+    Text('data'),
+    Text('data'),
+  ];
+  void _ontapped(int index)async{
+    setState(() {
+      _selectedIndex=index;
+    });
+  }
+  int _selectedIndex=0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: _widgetList.elementAt(_selectedIndex),),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+          onTap: _ontapped,
+          selectedItemColor: Colors.amber[800],
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home),label: ''),
+            BottomNavigationBarItem(icon: Icon(Icons.bar_chart),label: ''),
+            BottomNavigationBarItem(icon: Icon(Icons.person_rounded),label: ''),
+          ]),
+    );
+  }
+}
